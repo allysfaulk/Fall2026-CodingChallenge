d1ee5540ed6d677da210caf27ac334aa543dd0a2
Ally Faulk <ally.s.faulk@gmail.com>
2026-09-17 14:59:55 -0500

    Set up React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>My Image Collections</h1>
+      <p>Save and organize your favorite images.</p>
+
+      <button>Create Collection</button>
+    </div>
+  )
+}
+
+export default App
